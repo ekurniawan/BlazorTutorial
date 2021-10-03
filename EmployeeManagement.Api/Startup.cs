5fcb78d38a840710094082c338f7e01d5f8b0cc5
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-10-03 18:59:58 +0700

    koreksi dependency injection

--- EmployeeManagement.Api/Startup.cs
+++ EmployeeManagement.Api/Startup.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeManagement.Api.Data;
+using EmployeeManagement.Api.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +28,10 @@ namespace EmployeeManagement.Api {
         public void ConfigureServices (IServiceCollection services) {
 
             services.AddControllers ();
+            services.AddDbContext<AppDbContext> (options =>
+                options.UseSqlServer (Configuration.GetConnectionString ("DefaultConnection")));
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+                    
             services.AddSwaggerGen (c => {
                 c.SwaggerDoc ("v1", new OpenApiInfo { Title = "EmployeeManagement.Api", Version = "v1" });
             });
@@ -33,11 +40,10 @@ namespace EmployeeManagement.Api {
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
-           
-                app.UseDeveloperExceptionPage ();
-                app.UseSwagger ();
-                app.UseSwaggerUI (c => c.SwaggerEndpoint ("/swagger/v1/swagger.json", "EmployeeManagement.Api v1"));
-            
+
+            app.UseDeveloperExceptionPage ();
+            app.UseSwagger ();
+            app.UseSwaggerUI (c => c.SwaggerEndpoint ("/swagger/v1/swagger.json", "EmployeeManagement.Api v1"));
 
             app.UseHttpsRedirection ();
 
